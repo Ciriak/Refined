d8e9ed2282c98e3f3971cba58bdae7a5d9f1b725
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-08-28 13:48:37 +0200

    Moved more method to the "server side" (performance optimization)

--- src/tf2.js
+++ src/tf2.js
@@ -39,35 +39,101 @@ function reloadItemSchema(version, itemsGameUrl){
 
 //when client ask server list
 ipc.on('retreiveServers', function (data) {
+  console.log("Retreiving servers...");
   //retreive servers list
   var filters = "";
-  var nand = null;
-  if(data.filters.nand){
-    nand = data.filters.nand;
+  var customFilters = null;
+  if(data.filters.tags){
+    customFilters = data.filters.tags;
   }
   for (var prop in data.filters) {
-    if (data.filters.hasOwnProperty(prop) && prop !== "nand") {
+    if (data.filters.hasOwnProperty(prop) && prop !== "tags") {
       var val = data.filters[prop];
       filters = filters+formatFilter(prop, val);
     }
   }
 
-  //check the nand props
-  if(nand !== null){
-    for (var prop in nand) {
-      if (nand.hasOwnProperty(prop)) {
-        var val = nand[prop];
-        filters = filters+"\\nand"+formatFilter(prop, val);
+
+
+  client.getServerList(filters, data.limit, function(servers){
+    console.log(servers.length+" server(s) found, filtering...");
+    var newServers = [];
+    for (var i = 0; i < servers.length; i++) {
+      //Ignore the server if already known
+      if(_.findIndex(knownServers, { 'name' : servers[i].name }) === -1){
+
+        //check the tags of the server
+        if(checkCustomFilters(servers[i].gametype, customFilters)){
+
+          //insert additionnals informations and add the server to the new list
+          servers[i].mapName = getMapName(servers[i].map);
+          servers[i].gameMode = getGameModeName(servers[i].map);
+          newServers.push(servers[i]);
+
+        }
       }
     }
-  }
 
-  console.log(filters);
-  client.getServerList(filters, data.limit, function(servers){
-    ipc.emit("serversList", servers);
+    console.log(newServers.length+" new server(s) send");
+    //check if server is known
+    ipc.emit("serversList", newServers);
   });
 });
 
+function checkCustomFilters(tags, customFilter){
+  //create empty tags if needed (prevent error)
+  if(!tags){
+    tags = "";
+  }
+  var tagsList = tags.split(",");
+  for (var i = 0; i < tagsList.length; i++) {
+    var fi = _.indexOf(customFilter.exclude, tagsList[i]);
+    if(fi !== -1){
+      return false;
+    }
+  }
+  return true;
+}
+
+function getMapName(map){
+  if(!map){   //stop if map name not provided
+    return false;
+  }
+  if(refined.maps[map]){
+    if(refined.maps[map].label){
+      return refined.maps[map].label;  //return map custom label if exist
+    }
+  }
+  return map;   //return normal label (ex ctf_xxx)
+}
+
+function getGameModeName(map, isShort){
+  if(!map){   //stop if map name not provided
+    return false;
+  }
+  var short;
+  short = map.split("_");
+  short = short[0];
+  if(isShort){        //if shortName asked return it and stop
+    return short;
+  }
+  else{             //gamemode override by map name (ex mediaval for cp_degrootkeep)
+    if(refined.maps[map]){
+      if(refined.maps[map].gameMode){  //if specific gamemode exist for this map
+        return refined.maps[map].gameMode;
+      }
+    }
+    for (var gm in refined.gameModes) {
+      if (refined.gameModes.hasOwnProperty(gm)) {
+        var r = _.indexOf(refined.gameModes[gm], short);
+        if(r > -1){
+          return gm;
+        }
+      }
+    }
+    return "Unknow Gamemode";
+  }
+}
 
 function formatFilter(prop, val){
   return "\\"+prop+"\\"+val.toString().replace("true", "1").replace("false", "0");
--- src/web/js/controllers/server.js
+++ src/web/js/controllers/server.js
@@ -1,5 +1,6 @@
 app.controller('serverCtrl', function($scope, $rootScope, $stateParams)
 {
+  $scope.currentView = "list";
   $scope.list = {
     max : $rootScope.refined.servers.max,
     expected : $rootScope.refined.servers.max - 1,
@@ -59,49 +60,15 @@ app.controller('serverCtrl', function($scope, $rootScope, $stateParams)
     }
   };
 
-  $scope.getGamemodeName = function(map, isShort){
-    if(!map){   //stop if map name not provided
-      return false;
-    }
-    var short;
-    short = map.split("_");
-    short = short[0];
-    if(isShort){        //if shortName asked return it and stop
-      return short;
-    }
-    else{             //gamemode override by map name (ex mediaval for cp_degrootkeep)
-      if($rootScope.refined.maps[map]){
-        if($rootScope.refined.maps[map].gameMode){  //if specific gamemode exist for this map
-          return $rootScope.refined.maps[map].gameMode;
-        }
-      }
-      var r = $rootScope.refined.gameModes[short];
-      if(!r){
-        return "Unknow Gamemode";
-      }
-      return $rootScope.refined.gameModes[short];
-    }
-  };
-
-  $scope.getMapName = function(map){
-    if(!map){   //stop if map name not provided
-      return false;
-    }
-    if($rootScope.refined.maps[map]){
-      if($rootScope.refined.maps[map].label){
-        return $rootScope.refined.maps[map].label;  //return map custom label if exist
-      }
-    }
-    return map;   //return normal label (ex ctf_xxx)
-  };
-
   //master server query filter
   $scope.filters = {
     "appid": 440,
     "secure": true,
     "empty": false,
-    "nand": {           // not the following properties
-      "name_match": "Valve*"
+    "tags": {
+      "exclude": [
+        "valve"
+      ]
     }
   };
 
@@ -116,15 +83,12 @@ app.controller('serverCtrl', function($scope, $rootScope, $stateParams)
   $rootScope.ipc.on("serversList", function(data){
     $scope.retreiving = false;
     for (var i = 0; i < data.length; i++) {
-      var fi = _.findIndex($scope.list.servers, { 'name' : data[i].name });
-      if(fi === -1 && $scope.list.servers.length < $scope.list.max){
+      if($scope.list.servers.length < $scope.list.max){
         $scope.list.expected++;
+        $scope.list.servers.push(data[i]);
       }
       else{
         $scope.list.retreiving = false;
-        if(!$scope.$$phase) {
-          $scope.$apply();
-        }
       }
     }
 
